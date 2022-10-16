e737e745d6bb7f135a981fb1e4280a511664118e
Ihor Podvalniy <bembasua@gmail.com>
2022-10-16 17:03:27 +0300

    bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import { Component, useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState, useRef } from 'react';
 import { Box } from './Box/Box';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -8,131 +7,99 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import './App.css';
-
-const API_KEY = '29476807-778104ca63f185ac7ce275560';
-
-let filteredImages = [];
-
-const pageSize = 12;
+import { fetchImages } from 'utils/fetchImages';
 
 let selectedImage = '';
 
-export class App extends Component {
-  state = {
-    page: 1,
-    query: '',
-    images: [],
-    totalResults: 0,
-    status: 'idle',
-    showModal: false,
-  };
+export function App() {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
+  const [status, setStatus] = useState('idle');
+  const [showModal, setShowModal] = useState(false);
+  const isFirstRender = useRef(true);
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.setState({
-      page: 1,
-      query: e.target.elements.query.value,
-      images: [],
-      totalResults: 0,
-    });
-    filteredImages = [];
+    setPage(1);
+    setQuery(e.target.elements.query.value);
+    setImages([]);
+    setTotalResults(0);
+    e.target.reset();
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => {
+    setPage(page + 1);
   };
 
-  handleImageClick = url => {
-    this.toggleModal();
+  const handleImageClick = url => {
+    toggleModal();
     selectedImage = url;
   };
 
-  toggleModal = () => {
-    console.log('qwe');
-    this.setState({ showModal: !this.state.showModal });
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ status: 'pending' });
-      try {
-        axios
-          .get(
-            'https://pixabay.com/api/?image_type=photo&orientation=horizontal',
-            {
-              params: {
-                page: this.state.page,
-                key: API_KEY,
-                q: this.state.query,
-                per_page: pageSize,
-              },
-            }
-          )
-          .then(response => {
-            if (response) {
-              response.data.hits.map(image => {
-                filteredImages.push(image);
-                return filteredImages;
-              });
-
-              this.setState({
-                images: filteredImages,
-                totalResults: response.data.totalHits,
-                status: 'resolved',
-              });
-            }
-            if (response.data.hits.length <= 0) {
-              this.setState({ status: 'rejected' });
-            }
-          });
-      } catch (error) {
-        this.setState({ status: 'rejected' });
-        console.log(error);
-      }
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
     }
-  }
-
-  render() {
-    if (this.state.status === 'idle') {
-      return <Searchbar onSubmit={this.handleSubmit}></Searchbar>;
+    if (query === '') {
+      return;
     }
-    if (this.state.status === 'pending') {
-      return (
-        <Box className="App">
-          <Searchbar onSubmit={this.handleSubmit}></Searchbar>
+    setStatus('pending');
+
+    try {
+      fetchImages(page, query).then(response => {
+        if (response) {
+          setTotalResults(response.data.totalHits);
+          setStatus('resolved');
+          return setImages(prevImages => [
+            ...prevImages,
+            ...response.data.hits,
+          ]);
+        }
+        if (response.data.hits.length <= 0) {
+          setStatus('rejected');
+        }
+      });
+    } catch (error) {
+      setStatus('rejected');
+      console.log(error);
+    }
+  }, [page, query]);
+
+  return (
+    <Box className="App">
+      <Searchbar onSubmit={handleSubmit}></Searchbar>
+      {status === 'pending' && (
+        <>
           <ImageGallery>
-            <ImageGalleryItem data={this.state.images}></ImageGalleryItem>
+            <ImageGalleryItem data={images}></ImageGalleryItem>
           </ImageGallery>
           <Loader />
-        </Box>
-      );
-    }
-    if (this.state.status === 'resolved') {
-      return (
-        <Box className="App">
-          {this.state.showModal && (
-            <Modal url={selectedImage} onClose={this.toggleModal}></Modal>
+        </>
+      )}
+      )
+      {status === 'resolved' && (
+        <>
+          {showModal && (
+            <Modal url={selectedImage} onClose={toggleModal}></Modal>
           )}
-          <Searchbar onSubmit={this.handleSubmit}></Searchbar>
           <ImageGallery>
             <ImageGalleryItem
-              onClick={this.handleImageClick}
-              data={this.state.images}
+              onClick={handleImageClick}
+              data={images}
             ></ImageGalleryItem>
           </ImageGallery>
-          {this.state.images.length < this.state.totalResults && (
-            <Button onClick={this.loadMore} page={this.state.page}></Button>
+          {images.length < totalResults && (
+            <Button onClick={loadMore} page={page}></Button>
           )}
-        </Box>
-      );
-    }
-    if (this.state.status === 'rejected') {
-      return <Searchbar onSubmit={this.handleSubmit}></Searchbar>;
-    }
-  }
+        </>
+      )}
+    </Box>
+  );
 }
